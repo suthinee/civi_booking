54272a77a9897d72db2cee0ad30719a8d9d04f8f
Erawat Chamanont <erawat@gmail.com>
2012-05-29 18:16:59 +0100

    add CRM_Booking_Page_Slot page

---
+++ src/CRM/Booking/Page/Slot.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2011
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Page.php';
+
+/**
+ * Event Info Page - Summmary about the event
+ */
+class CRM_Booking_Page_Slot extends CRM_Core_Page{
+
+    /**
+     * Run the page.
+     *
+     * This method is called after the page is created. It checks for the  
+     * type of action and executes that action.
+     * Finally it calls the parent's run method.
+     *
+     * @return void
+     * @access public
+     *
+     */
+    function run() {
+          
+        return parent::run();
+        
+    }
+
+}
+
