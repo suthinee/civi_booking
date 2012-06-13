c9347d64d04c76d95a2bdb81acf03a22d44278f8
Erawat Chamanont <erawat@gmail.com>
2012-06-13 18:40:42 +0100

    add service session to query

--- src/CRM/Booking/BAO/Slot.php
+++ src/CRM/Booking/BAO/Slot.php
@@ -14,7 +14,7 @@ class CRM_Booking_BAO_Slot{
         2 => array( $endDate, 'String')
           );
 
-      $query = "SELECT contact_id, start_time,  end_time, room_no, slot_date
+      $query = "SELECT contact_id, start_time,  end_time, room_no, slot_date, session_service
         FROM civi_booking_slot
         LEFT JOIN civi_booking_room ON civi_booking_room.id = civi_booking_slot.room_id
         WHERE slot_date BETWEEN %1 AND %2";  
@@ -29,9 +29,8 @@ class CRM_Booking_BAO_Slot{
 
     }
 
-    static function getSlotByCounsellorId($counsellorId, $activityType, $startDate = null, $endDate = null){
-      $params = array(1 => array( $counsellorId, 'Integer'),
-                      2 => array( $activityType, 'Integer'));
+    static function getSlots($activityType, $startDate = null, $endDate = null){
+      $params = array(1 => array( $activityType, 'Integer'));
       $query = "SELECT civi_booking_slot.id, 
                       start_time as start_time,
                       end_time as end_time,
@@ -40,15 +39,14 @@ class CRM_Booking_BAO_Slot{
                       sort_name as sort_name,
                       display_name as display_name,
                       session_service as session_service,
-                      label as activity_name
+                      label as activity_name,
+                      status as status
         FROM civi_booking_slot
         LEFT JOIN civi_booking_room ON civi_booking_room.id = civi_booking_slot.room_id
         LEFT JOIN civicrm_contact ON civicrm_contact.id = civi_booking_slot.contact_id
         LEFT JOIN civicrm_option_value ON civicrm_option_value.value = civi_booking_slot.activity_type
         WHERE civicrm_option_value.option_group_id = 2 
-        AND civi_booking_slot.contact_id = %1
-        AND civi_booking_slot.activity_type = %2
-        AND civi_booking_slot.status = 1";  
+        AND civi_booking_slot.activity_type = %1";  
          
       require_once('CRM/Core/DAO.php'); 
       $dao = CRM_Core_DAO::executeQuery( $query,  $params );
