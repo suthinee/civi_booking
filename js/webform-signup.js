df753fee8de2c747fe433f05b98f536848d78b34
Erawat Chamanont <erawat@gmail.com>
2012-07-02 21:54:36 +0100

    slot slection works

--- js/webform-signup.js
+++ js/webform-signup.js
@@ -5,28 +5,10 @@
 		if($("#edit-submitted-select-a-slot").length > 0){
 			$("#edit-submitted-select-a-slot").attr("disabled", "disabled");
 		}
-		
-		/*
-		$("#edit-submitted-select-a-slot").click(function() {
-			var service = $('select[name="submitted[disabled_1_activity_1_cg7_custom_15]"]').val(); 
-			var centre = $('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
-			if(service == '' && centre == ''){
-				alert('Please select service and prefered centre');
-			}else if(service == ''){
-				alert('Please select service');
-			}else if(centre == ''){
-				alert('Please select centre');
-			}else{
-				//var basePath = window.location.hostname + '/';
-				var url = 'apis/slot/get/1/2/4/4'
-				$.getJSON(url, function(data){
-					alert(data);
-				});
-			}
-		});
-		*/
+
 
 		$('#edit-submitted-civicrm-1-activity-1-cg7-custom-15').change(function(){
+			clearSlot();
 			var centre = $('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
 			if(centre != '' && $(this).val != ''){
 				generateAvailableSlots();
@@ -37,6 +19,7 @@
 		});
 
 		$('#edit-submitted-civicrm-1-case-1-cg6-custom-9').change(function(){
+			clearSlot();
 			var service = $('select[name="submitted[civicrm_1_activity_1_cg7_custom_15]"]').val(); 
 			if(service != '' && $(this).val != ''){
 				generateAvailableSlots();
@@ -47,24 +30,32 @@
 			}	
 		});
 
+	function clearSlot(){
+		$("#edit-submitted-select-a-slot").attr("disabled", "disabled");
+		var options = '<option value="" selected="selected">- Select -</option>';
+		options += '<option value="0">Unable to find a slot, Request a call back</option>';
+		$("#edit-submitted-select-a-slot").html(options);
+	}	
+	
 		
 	function generateAvailableSlots(){
 		var centre = $('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
 		var service = $('select[name="submitted[civicrm_1_activity_1_cg7_custom_15]"]').val(); 
-		var basePath = window.location.hostname + '/';
-		console.log(basePath);
-		var url = "/tccr/slot/get/" + service + "/" + centre;
-		console.log(url);
+		var basePath = Drupal.settings.basePath;
+		var url = basePath + "slot/get/" + service + "/" + centre;
+		var slotList = $('#edit-submitted-select-a-slot');
 		$.getJSON(url, function(data){
-				console.log(data);
+			if(data.is_error == 0){
+				var slots = data.results;
+				for(var slotId in slots){
+					optionVal = '<option value="'+ slotId +'">'+ slots[slotId].slot_day + ' - ' + slots[slotId].slot_date + ', ' + slots[slotId].start_time + ' - ' + slots[slotId].end_time + '</option>';
+					slotList.append(optionVal);
+				}
+			}
 		});
 
-	}
-
-		//var basePath = window.location.hostname + '/';
+	}		
 		
-		
-
 	});
 })(jQuery);
 
