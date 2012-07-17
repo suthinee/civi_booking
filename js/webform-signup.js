645d3a5d8dd0c14bc3c6908b69a01e0510db36a8
Erawat Chamanont <erawat@gmail.com>
2012-07-17 11:10:27 +0100

    updated css and js

--- js/webform-signup.js
+++ js/webform-signup.js
@@ -1,17 +1,19 @@
 function createForm(ms){
 	clearSlot();
 	jQuery(".loading").show();
-	var centre = jQuery('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
-	var service = jQuery('select[name="submitted[civicrm_1_activity_1_cg7_custom_15]"]').val(); 
+	//var centre = jQuery('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
+	//var service = jQuery('select[name="submitted[civicrm_1_activity_1_cg7_custom_15]"]').val(); 
 	var basePath = Drupal.settings.basePath;
-	var url = basePath + "slot/get/" + service + "/" + centre;
+	//var url = basePath + "slot/get/" + service + "/" + centre;
+	var url = basePath + "slots/get";
 	var now = new Date();
 	if(ms != undefined) url += "/" + ms;
 	var component = jQuery('#webform-component-seleted-slot-id');
 	component.append("<div class='slot'></div>");
 	component = jQuery('#webform-component-seleted-slot-id .slot');
-	if(centre != '' && service != ''){
+	//if(centre != '' && service != ''){
 		jQuery.getJSON(url, function(data){
+			console.log(data);
 			var check_unable = false;
 			if(data.is_error == 0){
 				var slots = data.results;
@@ -19,21 +21,23 @@ function createForm(ms){
 				var end = new Date((data.nextWeek-86400)*1000);
 				var show = start.getDayName() + " " + start.getUTCDate() + " " + start.getMonthName() + " - " + end.getDayName() + " " + end.getUTCDate() + " " + end.getMonthName() + " " + end.getUTCFullYear();
 				component.append("<div class='change-week'><span class='pre' onclick='createForm("+data.lastWeek+");'><</span><span class='show'>"+show+"</span><span class='next' onclick='createForm("+data.nextWeek+");'>></span></div>");
-				// component.append("<div class='options'>Show booked <input type='radio' name='mark' class='show' CHECKED> | Hide booked <input type='radio' name='mark' class='hide' ></div>");
+				component.append("<div class='options'>Show booked <input type='radio' name='mark' class='show' CHECKED> | Hide booked <input type='radio' name='mark' class='hide' ></div>");
+				component.append("<ul class='group'><li class='head item'><div class='first left'>Date</div><div class='middle left'>Time</div><div class='last left'>Select</div></li></ul>");
 				
-				if(ms >= now.getTime()){
-					component.append("<ul class='group'><li class='head item'><div class='first left'>Date</div><div class='middle left'>Time</div><div class='last left'>Select</div></li></ul>");
-				}else{
-					component.append("<ul class='group'><li class='head item'><div class='first left'>Date</div><div class='middle left'>Time</div></li></ul>");
-				}
 				var mark = false;
 				for(var slotId in slots){
 					mark = true;
-					var temp = "<li class='item'>";
+					booked = '';
+					if(slots[slotId].slot_date != null && slots[slotId].slot_date != ''){
+						booked = "booked";
+					}
+					var temp = "<li class='item "+ booked +"'>";
 					temp += "<div class='first left'>" + slots[slotId].slot_day + "<br/>" + slots[slotId].slot_date  + "</div>";
 					temp += "<div class='middle left'>" + slots[slotId].start_time + " - " + slots[slotId].end_time  + "</div>";
 					if(ms >= now.getTime()){
 						temp += "<div class='last left'><input type='radio' name='group' value='"+slotId+"'></div>";
+					}else{
+						temp += "<div class='last left'><input type='radio' name='group' value='"+slotId+"' disabled></div>";
 					}
 					temp += "</li>";
 					jQuery("ul",component).append(temp);
@@ -55,7 +59,7 @@ function createForm(ms){
 				jQuery("ul.group",component).append('<li class="item">Unable to find a slot? Request a call back <input id="unable-callback" type="checkbox" onclick="setUnableSlot(0);" /></li>');
 			}
 		});
-	}
+	//}
 }
 
 function setUnableSlot(value){
@@ -85,8 +89,11 @@ function bindRadio(){
 (function ($) {
 	$(document).ready(function () { 
 
-		$("#webform-component-civicrm-1-case-1-cg6-custom-9").append("<div class='loading'></div>");
+		createForm();
 
+
+		$("#webform-component-civicrm-1-case-1-cg6-custom-9").append("<div class='loading'></div>");
+		/*
 		$('#edit-submitted-civicrm-1-activity-1-cg7-custom-15').change(function(){
 			var centre = $('select[name="submitted[civicrm_1_case_1_cg6_custom_9]"]').val(); 
 			if(centre != '' && $(this).val != ''){
@@ -103,7 +110,7 @@ function bindRadio(){
 
 		if($('#edit-submitted-civicrm-1-case-1-cg6-custom-9').val() != 0 && $('#edit-submitted-civicrm-1-activity-1-cg7-custom-15').val() != 0){
 			createForm();
-		}
+		}*/
 
 	});
 })(jQuery);
